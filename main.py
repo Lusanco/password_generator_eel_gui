6516fc6dd4f6cad877edfdb4d2e57cd224fce9b3
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-02-04 12:30:30 -0400

    setup - initial setup

---
+++ main.py
@@ -0,0 +1,9 @@
+import eel
+import json
+
+history_count = 0
+
+eel.init("web")
+
+
+eel.start("index.html")
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./web/*.{html, js, py}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
